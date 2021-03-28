87c267017c1e855bbdcfcd77db7816a293fdb453
Ryan <trbuck13@gmail.com>
2021-03-27 23:16:14 -0500

    Update credentials.js

--- src/credentials.js
+++ src/credentials.js
@@ -3,9 +3,9 @@ const config = {
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DB_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: "planning-poker-3326c.appspot.com",
-  messagingSenderId: "873603982878",
-  appId: "1:873603982878:web:771bcf8f7569c7b31f300f"
-}
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
 
-export default config
+export default config;
